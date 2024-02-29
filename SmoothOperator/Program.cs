713947dc9ad6c927cbbd8baa7230f907a0ac6bbe
a22007432 <ssm.antonio@gmail.com>
2024-02-29 15:02:56 +0000

    Exercicio 1 feito

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Insere número inteiro não-negativo:");
+            
+            
+            string sn = Console.ReadLine();
+            int intn = int.Parse(sn);
+
+            byte n = (byte) intn;
+
+            //Console.WriteLine(n);
+            byte n1 = --n; 
+            Console.WriteLine(n1);
+            byte n2 = ++n1;
+
+            //Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            
+            
+
+
+        }
+    }
+}

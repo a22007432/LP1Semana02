8240e0de7deb24e5592f67a505f128f714cbc600
a22007432 <ssm.antonio@gmail.com>
2024-02-29 15:23:27 +0000

    added ex4

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -12,17 +12,26 @@ namespace SmoothOperator
             
             
             string sn = Console.ReadLine();
-            int intn = int.Parse(sn);
+            //int intn = int.Parse(sn);
 
-            byte n = (byte) intn;
+            byte n = byte.Parse(sn);
 
             //Console.WriteLine(n);
-            byte n1 = --n; 
-            Console.WriteLine(n1);
-            byte n2 = ++n1;
+            //byte n1 = --n; 
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+            //byte n2 = ++n1;
 
             //Console.WriteLine(n1);
-            Console.WriteLine(n2);
+            //Console.WriteLine(n2);
+
+            
+
+            //Console.WriteLine($"this is n : ->{n}");
+            Console.WriteLine((n/2));
+            Console.WriteLine((n<<3));
+            Console.WriteLine((n^5));
+            Console.WriteLine((n>10));
             
             
 

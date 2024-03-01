fec0bcd134512b6719988eb2ee40ec3f37396961
a22007432 <ssm.antonio@gmail.com>
2024-03-01 17:14:55 +0000

    step 1 done

---
+++ MasterAndPlayer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MasterAndPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 0;
+            bool correct = false;
+            bool validn = false;
+
+
+            
+
+            do
+            { 
+
+
+                Console.WriteLine("Number Master insert number: ");
+                int n = int.Parse(Console.ReadLine());
+                if (n < 0 || n > 100)
+                {
+                    Console.WriteLine("invalid number try again");
+
+                }
+                else
+                {
+                    validn = true;
+                }
+            }while(validn == false);
+
+
+            
+
+            /*if (n < 0 || n > 100)
+            {
+                Console.WriteLine("invalid number get out");
+            }
+            else
+            {
+                
+                do
+                {
+                    if(correct == false)
+                    {
+                        
+                    }
+
+                } while(correct == false);
+            }*/
+        }
+    }
+}

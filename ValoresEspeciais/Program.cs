2e2ef2bf8d280bcc81cd15ce3d798e7f4ddc9b66
a22007432 <ssm.antonio@gmail.com>
2024-03-01 15:02:13 +0000

    added min maxing to coding

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,35 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int i = int.MaxValue;
+            int i_s = int.MinValue;
+
+            float f = float.MaxValue;
+            float f_s = float.MinValue;
+
+            double d = double.MaxValue;
+            double d_s = double.MinValue;
+
+            byte b = byte.MaxValue;
+            byte b_s = byte.MinValue;
+            
+            short s = short.MaxValue;
+            short s_s = short.MinValue;
+
+            Console.WriteLine(i);
+            Console.WriteLine(i_s);
+
+            Console.WriteLine(d);
+            Console.WriteLine(d_s);
+
+            Console.WriteLine(b);
+            Console.WriteLine(b_s);
+
+            Console.WriteLine(s);
+            Console.WriteLine(s_s);
+
+            Console.WriteLine(f);
+            Console.WriteLine(f_s);
         }
     }
 }
